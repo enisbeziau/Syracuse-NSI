a6bb2ae2fc9f03c0f8b3d456b52076019fd55052
enisbeziau <aristo2110@gmail.com>
2023-10-27 17:52:33 +0200

    Ajout du bouton pour mettre le graphique en mode semi-logarithmique

--- js/app.js
+++ js/app.js
@@ -17,10 +17,17 @@ import {
 
 // Constantes et variable utilisées pour la partie affichage du graphique
 const canvas = document.getElementById("graphique");
-const btn = document.getElementById("btn");
+const btn_input = document.getElementById("btn");
 const entree = document.getElementById("input");
 const zoneGraphique = document.getElementById("zone-graphique");
 let premierClic = true;
+
+
+// Constantes pour la gestion des boutons
+const divBtn = document.getElementById('ensemble-btn');
+const boutons = document.querySelectorAll('.btn');
+const btnOrtho = document.getElementById('btn-ortho');
+const btnLog = document.getElementById('btn-log');
 
 
 // Constantes utilisées pour la partie informations
@@ -32,10 +39,25 @@ const altitudeMax = document.getElementById("altitude-max");
 const facteurExpansion = document.getElementById("facteur-expansion");
 
 
-btn.addEventListener('click', () => {
+// Gestion du changement de couleur au clic d'un bouton --> ajoute un écouteur d'événement de clic à chaque bouton
+boutons.forEach(bouton => {
+    bouton.addEventListener('click', () => {
+        // Réinitialise la couleur de fond de tous les boutons
+        boutons.forEach(boutonReinitialise => {
+            boutonReinitialise.style.backgroundColor = '';
+        });
+        
+        // Change la couleur de fond du bouton cliqué en bleu
+        bouton.style.backgroundColor = "#1b9bff";
+    });
+});
+
+
+btn_input.addEventListener('click', () => {
     const entreeValide = parseInt(entree.value);
 
     if (verifierEntree(entreeValide)) {
+
         const ctx = canvas.getContext('2d');
         let suiteSyracuse = syracuseSuite(entreeValide);
         let suiteCompresseeSyracuse = syracuseCompresseeSuite(entreeValide)
@@ -49,15 +71,29 @@ btn.addEventListener('click', () => {
         if (premierClic) {
             zoneGraphique.style.display = "block";
             sectionInformations.style.display = "block";
+            divBtn.style.display = "flex";
             premierClic = false;
         }
 
+
+        btnOrtho.addEventListener('click', () => {
+            supprimerGraphiqueExistant(canvas);
+            creerGraphique(ctx, suiteSyracuse, "linear")
+        })
+
+
+        btnLog.addEventListener('click', () => {
+            supprimerGraphiqueExistant(canvas);
+            creerGraphique(ctx, suiteSyracuse, "logarithmic");
+        })
+        
+
+        // Remplissage des informations de la section informations en fonction de l'entrée de l'utilisateur
         vol.innerHTML = "Vol de la suite : " + suiteSyracuse.length;
         altitudeMax.innerHTML = "Altitude maximale : " + Math.max(...suiteSyracuse);
         facteurExpansion.innerHTML = "Facteur d'expansion : " + Math.round((Math.max(...suiteSyracuse) / entreeValide));
         suiteTxt.innerHTML = suiteSyracuse;
         txtCompressee.innerHTML = suiteCompresseeSyracuse;
-    } else {
-        alert("Vous devez entrer un nombre entier supérieur ou égal à 1")
-    }
+    } 
+    else { alert("Vous devez entrer un nombre entier supérieur ou égal à 1");}
 });
