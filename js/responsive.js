b2a84f37d87732c078a97da7417f4c561080d614
= <aristo2110@gmail.com>
2023-11-17 17:41:39 +0100

    Optimisation du site pour mobile

---
+++ js/responsive.js
@@ -0,0 +1,14 @@
+/**
+ * Auteur : Enis Béziau
+ * Ce fichier sert à appliquer des classes à des élements css lors du clic sur le menu hamburger
+ * Sert à gérer le responsive de la navbar et son changement d'affichage sur les écrans <632px
+ */
+const hamburger = document.querySelector(".hamburger");
+const lienNav = document.querySelector(".ensemble-lien");
+
+
+hamburger.addEventListener('click', () => {
+    lienNav.classList.toggle("menu-mobile");
+    document.documentElement.classList.toggle("no-scroll");
+
+})
